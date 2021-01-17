eed0a2c09086fc2feff7f8c912402ffcf784650d
Cecep Solihin Yusup <cecepsolihinyusup@gmail.com>
2021-01-17 16:46:49 +0700

    vue routers tutorials

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, iure ex fugit dolor expedita in voluptas maiores nesciunt dolorem ut doloremque nihil. Maiores eos perferendis, repellendus debitis suscipit cupiditate, delectus maxime laudantium quos deleniti, excepturi fugit dicta consectetur consequatur. Eveniet, totam accusamus molestias omnis veniam aperiam dolorum debitis repudiandae, facere assumenda ipsa quia nam nulla obcaecati. Quo assumenda nihil repellat quae dolor libero repellendus id velit similique! Expedita provident vero vel animi. Et dolore sunt iste consectetur quis aliquid laudantium facilis, ipsum numquam dolorem molestias, doloremque sit. Tempore corrupti maxime tenetur voluptatem nam accusantium, nulla quaerat fugit laboriosam aperiam. At.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Homepage</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem commodi voluptate atque mollitia consectetur nisi, sequi numquam quibusdam incidunt aut quae dolores autem dignissimos aliquam nulla sit ad. Reiciendis voluptates vel fugit, tenetur earum voluptate voluptatum tempore culpa corrupti illum? Adipisci quasi nam aliquam eius nostrum aperiam error repellendus labore consectetur sequi ab nemo sint voluptatem aspernatur eaque, omnis quo! Repellendus, nihil esse. Voluptatum inventore totam maxime necessitatibus culpa magnam, accusamus similique perferendis deserunt veritatis, amet, assumenda ipsa reiciendis laudantium ad blanditiis consequatur libero dolores obcaecati repudiandae consequuntur! Ut eius a quae. Assumenda odit excepturi aliquid mollitia officia modi a!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem commodi voluptate atque mollitia consectetur nisi, sequi numquam quibusdam incidunt aut quae dolores autem dignissimos aliquam nulla sit ad. Reiciendis voluptates vel fugit, tenetur earum voluptate voluptatum tempore culpa corrupti illum? Adipisci quasi nam aliquam eius nostrum aperiam error repellendus labore consectetur sequi ab nemo sint voluptatem aspernatur eaque, omnis quo! Repellendus, nihil esse. Voluptatum inventore totam maxime necessitatibus culpa magnam, accusamus similique perferendis deserunt veritatis, amet, assumenda ipsa reiciendis laudantium ad blanditiis consequatur libero dolores obcaecati repudiandae consequuntur! Ut eius a quae. Assumenda odit excepturi aliquid mollitia officia modi a!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
